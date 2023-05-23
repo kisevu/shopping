9931eaf4bc13fd78afed10481e5312a0e01d22a7
ameda-kevin <amedakevin@gmail.com>
2023-05-23 15:59:44 +0300

    updated the backend functionality and the logo now is clickable and redirects to the homer page

--- ShopWebParent/ShopBackend/src/main/java/com/amedakevin/app/webParent/admin/ShopBackendApplication.java
+++ ShopWebParent/ShopBackend/src/main/java/com/amedakevin/app/webParent/admin/ShopBackendApplication.java
@@ -1,11 +1,11 @@
-package com.amedakevin.app.webParent;
+package com.amedakevin.app.webParent.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShopFrontendApplication {
+public class ShopBackendApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ShopFrontendApplication.class,args);
+        SpringApplication.run(ShopBackendApplication.class,args);
     }
 }
--- ShopWebParent/ShopFrontend/src/main/java/com/amedakevin/app/webParent/site/ShopFrontendApplication.java
+++ ShopWebParent/ShopFrontend/src/main/java/com/amedakevin/app/webParent/site/ShopFrontendApplication.java
@@ -1,11 +1,11 @@
-package com.amedakevin.app.webParent;
+package com.amedakevin.app.webParent.site;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShopBackendApplication {
+public class ShopFrontendApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ShopBackendApplication.class,args);
+        SpringApplication.run(ShopFrontendApplication.class,args);
     }
 }

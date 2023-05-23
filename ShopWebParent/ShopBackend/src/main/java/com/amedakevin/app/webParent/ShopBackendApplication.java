ffa1c078f4f4448176ea7c2c40c520d900ce5594
ameda-kevin <amedakevin@gmail.com>
2023-05-23 12:23:41 +0300

    updated the pom.xml and everything runs as expected

---
+++ ShopWebParent/ShopBackend/src/main/java/com/amedakevin/app/webParent/ShopBackendApplication.java
@@ -0,0 +1,11 @@
+package com.amedakevin.app.webParent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShopBackendApplication.class,args);
+    }
+}

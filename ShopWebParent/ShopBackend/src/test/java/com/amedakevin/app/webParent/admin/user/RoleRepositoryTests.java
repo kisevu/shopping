b6b5f21b1e4fea476dc0b01162adb3c316d728c8
ameda-kevin <amedakevin@gmail.com>
2023-05-23 18:37:20 +0300

    included the @Rollback(false) annotation which persists data while testing(to be removed).

--- ShopWebParent/ShopBackend/src/test/java/com/amedakevin/app/webParent/admin/user/RoleRepositoryTests.java
+++ ShopWebParent/ShopBackend/src/test/java/com/amedakevin/app/webParent/admin/user/RoleRepositoryTests.java
@@ -5,11 +5,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.LIST;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
 public class RoleRepositoryTests {
     @Autowired
     private RoleRepository roleRepository;
@@ -21,5 +26,27 @@ public class RoleRepositoryTests {
                 .build();
        Role savedRole=roleRepository.save(roleAdmin);
        assertThat(savedRole.getId()).isGreaterThan(0);
+    }
+    @Test
+    public void testCreateRestRoles(){
+        Role roleSalesPerson=Role.builder()
+                .name("Salesperson")
+                .description("manage product price, customers, shipping," +
+                        "orders and sales repor")
+                .build();
+        Role roleEditor=Role.builder()
+                .name("Editor")
+                .description("manage categories, brands,products," +
+                        "articles and menus")
+                .build();
+        Role roleShipper=Role.builder()
+                .name("Shipper")
+                .description("view products, view orders and update order status")
+                .build();
+        Role roleAssistant=Role.builder()
+                .name("Assistant")
+                .description("manage questions and reviews")
+                .build();
+        roleRepository.saveAll(List.of(roleSalesPerson,roleEditor,roleShipper,roleAssistant));
     }
 }
